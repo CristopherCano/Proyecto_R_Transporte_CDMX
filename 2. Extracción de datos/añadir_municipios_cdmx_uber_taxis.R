1526f9b6b3de70a62fa49b88e0c86098f2bd3bef
Cristopher Cano <crisarvizu16@gmail.com>
2021-02-06 16:06:12 -0600

    Reverse Geocoding

--- 2. Extracción de datos/añadir_municipios_cdmx_uber_taxis.R
+++ 2. Extracción de datos/añadir_municipios_cdmx_uber_taxis.R
@@ -1,57 +1,119 @@
-library(geonames)
+### Cargamos las librerias 
+library(geonames) ### Geonames es una libreria de libre acceso y requiere crear un cuenta
 library(ggplot2)
 library(dplyr)
 
 
-cdmx.rutas <- read.csv("CDMX_Rutas.csv")
+### Cargamos la base de datos
+cdmx.rutas <- read.csv("cdmx_transporte_raw.csv")
 
+### Vemos la información de la base de datos
 head(cdmx.rutas); tail(cdmx.rutas); summary(cdmx.rutas); dim(cdmx.rutas);
 
+### Seleccionamos los orgines y destino
 
-ggplot(cdmx.rutas, aes(x = trip_duration/60)) + geom_histogram(bins = 30, fill = "#ff325b") 
-ggplot(cdmx.rutas, aes(x = dist_km)) + geom_histogram(bins = 30)
-
-ggplot(cdmx.rutas, aes(x = trip_duration_hrs)) + geom_boxplot()
-ggplot(cdmx.rutas, aes(x = dist_km)) + geom_boxplot()
-
+### Origen
 origen <- cdmx.rutas[,c("pickup_longitude","pickup_latitude")]
-destino <- cdmx.rutas[,c("dropoff_longitude","dropoff_latitude")]
+head(origen)
 
-###Pruebas multiple geocoding
-#options(geonamesUsername="diegoar")
-#options(geonamesUsername="cristophercano")
-#options(geonamesUsername="crisarvizu")
-#options(geonamesUsername="dgil")
-options(geonamesUsername="alanvillasana")
-#options(geonamesUsername="pepetellez")
+### Destino
+destino <- cdmx.rutas[,c("dropoff_longitude","dropoff_latitude")]
+head(Destino)
+
+### Mediante GNfindNearbyPostalCodes realizamos una prueba para obter el municipio
+### Para utilizar geonames creamos una cuenta previamente
+options(geonamesUsername="cristophercano")
+
+### Municipio de origen
+(mun.origen.1<-GNfindNearbyPostalCodes(lat = origen[1,2], lng=origen[1,1],radius = "10", maxRows = "1", style = "MEDIUM"))
+
+### Almacenamos solo el nombre del municipio de origen
+(nombre.mun.o <- mun.origen.1$adminName2)
+
+##La variable que nos devuelve es adminName2
+(paste("municipio de origen: ",nombre.mun.o))
+
+### Municipio de destino
+(mun.destino.1<-GNfindNearbyPostalCodes(lat = destino[1,2], lng=destino[1,1], radius = "10", maxRows = "1", style = "MEDIUM"))
+
+### Almacenamos solo el nombre del municipio de origen
+(nombre.mun.d <- mun.destino.1$adminName2)
+
+##La variable que nos devuelve es adminName2
+(paste("municipio de destino: ",nombre.mun.d))
+
+### Podemos corrovorar esta información trazando la ruta en un mapa mediante leaflet y osrmRoute
+library(leaflet)
+library(osrm)
+
+a <- c(origen[1,1],origen[1,2])
+b <- c(destino[1,1],destino[1,2])
+
+r<-osrmRoute(src = a,
+             dst = b,
+             returnclass = "sf", overview = "full",
+             osrm.profile = "car")
+r
+
+plot(st_geometry(r), add = TRUE)
+
+# Mapa de municipios de la CDMX
+mapa_municipios <- st_read("https://github.com/JuveCampos/Shapes_Resiliencia_CDMX_CIDE/raw/master/Zona%20Metropolitana/EdosZM.geojson", quiet = T) %>% 
+  filter(CVE_ENT == "09")
+
+# Mapa de la entidad de la Ciudad de México
+mapa_cdmx <- st_read("https://github.com/JuveCampos/Shapes_Resiliencia_CDMX_CIDE/raw/master/Zona%20Metropolitana/EstadosZMVM.geojson", quiet = T)[3,]
+
+
+m <- leaflet() %>% 
+  addTiles() %>% 
+  #addProviderTiles("CartoDB.Positron") %>% 
+  addPolylines(data=r$geometry, opacity=1, weight = 3) %>%
+  addMarkers(lng=destino[1,1], lat=destino[1,2], popup = nombre.mun.d, label = nombre.mun.d) %>%
+  addPopups(lng=origen[1,1], lat=origen[1,2], nombre.mun.o,options = popupOptions(closeButton = FALSE)) %>%
+  addPopups(lng=destino[1,1], lat=destino[1,2], nombre.mun.d,options = popupOptions(closeButton = FALSE)) %>%
+  addMarkers(lng=origen[1,1], lat=origen[1,2], popup = nombre.mun.o, label = nombre.mun.o) %>%
+  addPolygons(data = mapa_municipios, 
+              color = "#444444",
+              weight = 2, 
+              opacity = 0.4,
+              fill = F,
+              label = ~as.character(NOM_MUN)) %>%
+  addPolygons(data = mapa_cdmx, 
+              color = "#444444",
+              weight = 4, 
+              opacity = 1,
+              fill = F
+  )
+
+m
+
+
+
+
+### El condigo siguiente nos ayudo a realizar la tarea anterior pero aplicandolo a los más de 16,000 datos
+
+
 # Initialize the data frame
 #####==========ORIGEN=============####
 
+# Se dividieron los datos ya que existe un limite de creditos disponibles por hora
 1:500
 501:1000
 1001:1500
-1501:2000
-2001:2500
-2501:3000
-3001:3500
-3501:4000
-4001:4500
-4501:5000
-5001:5500
-5501:6000
-6001:6500
-6501:7000
-7001:7500
-7501:8000
-8001:8500
+...
 8501:8625
 
 
-idx <- 3887
+# Ejemplo 
+idx <- 1:20
 
+# Asiganmos un rago para encontrar los municipios
 origen.sample <-origen[idx,]
 
+# Nos aseguramos que sean los datos que queremos
 head(origen.sample); tail(origen.sample); dim(origen.sample); 
+
 
 iter = 0 # Loop para obtener el nombre de los municipios de origen 
 for(i in idx){
@@ -60,12 +122,17 @@ for(i in idx){
   cdmx.rutas$municipios_origen[i] <- result
 }
 
-write.csv(cdmx.rutas,"cdmx_rutas_municipios.csv")
+#print(i) 
 
+# Resultado final de los municipios de origen encontrados
+head(cdmx.rutas$municipios_origen[idx])
 
 #####============DESTINO=============######
 
-idx <- 8502:8625
+
+# El procedimiento es el mismo que el anterior
+
+idx <- 1:20
 
 destino.sample <-destino[idx,]
 
@@ -79,78 +146,8 @@ for(i in idx){
 print(i)
 
 
+# Resultado final de los municipios destino encontrados
+head(cdmx.rutas$municipios_destino[idx])
 
-# Write a CSV file containing origAddress to the working directory
-
-check1<-GNfindNearby(lat = 19.46011816213774, lng=-99.08603668212892)
-check2<-GNfindNearbyPlaceName(lat = 19.46011816213774, lng=-99.08603668212892,radius = "10", maxRows = "2", style = "MEDIUM")
-check3<-GNfindNearbyPostalCodes(lat = 19.46011816213774, lng=-99.08603668212892,radius = "10", maxRows = "2", style = "MEDIUM")
-check4<-GNfindNearbyPostalCodes(lat = 19.43454, lng=-99.15186,radius = "10", maxRows = "2", style = "MEDIUM")
-check5<-GNfindNearbyPostalCodes(lat = 19.43266, lng=-99.15497,radius = "10", maxRows = "2", style = "MEDIUM")
-(check6<-GNfindNearbyPostalCodes(lat = 19.38224, lng=-99.20050,radius = "10", maxRows = "1", style = "MEDIUM"))
-
-
-##visualization 
-
-library(leaflet)
-
-# create awesome icons
-my_icons <- awesomeIcons(icon = data_all$icon,
-                         markerColor = data_all$color,
-                         library = "glyphicon")
-
-
-m <- leaflet() %>%
-  addTiles() %>%  # Add default OpenStreetMap map tiles
-  addMarkers(lng=origen[1:100,1], lat=origen[1:100,2],
-             icon = list(iconUrl = 'https://icons.iconarchive.com/icons/artua/star-wars/256/Master-Joda-icon.png',
-               iconSize = c(75, 75)))
-             
-m
-
-base_de_datos <- st_read("http://rir.geoint.mx/RIR/archivos/101_coloniasCDMX.geojson")
-
-m <- leaflet(mapaFactor, options = leafletOptions(zoomControl = FALSE, minZoom = 10)) %>%
-  addProviderTiles("CartoDB.Positron") %>%
-  setMaxBounds(lng1 = -99.507504, lat1 = 18.980224, lng2 = -98.722426, lat2 = 19.60) %>%
-  addPolygons(highlightOptions = highlightOptions(color = "white"),
-              color = "#444444", 
-              weight = 0.2, 
-              smoothFactor = 0.5, 
-              opacity = 1, 
-              popup = popup,
-              label = lapply(label, htmltools::HTML), 
-              fillOpacity = 0.8, 
-              fillColor = "#FFFFE0" ) %>%
-  addPolygons(data = mapa_cdmx, 
-              color = "#444444",
-              weight = 4, 
-              opacity = 1,
-              fill = F) %>% 
-  addPolygons(data = mapa_municipios, 
-              color = "#444444",
-              weight = 2, 
-              opacity = 1,
-              fill = F) %>% 
-  addCircleMarkers(lng=origen[1:5000,1], lat=origen[1:5000,2], radius = 1, opacity = 0.05) %>%
-  addCircleMarkers(lng=destino[1:5000,1], lat=destino[1:5000,2], radius = 1, color = "#ff325b", opacity = 0.05) 
-
-m
-
-#addProviderTiles("Stamen.Watercolor")
-#addProviderTiles(providers$Esri.WorldImagery)
-#addProviderTiles(providers$OpenTopoMap)
-#addProviderTiles(providers$OpenStreetMap)
-#addProviderTiles("CartoDB.Positron")
-#addProviderTiles("Esri.WorldTerrain")
-
-###Agrupando
-
-library("maps")
-library("ggplot2")
-
-ggplot(cdmx.rutas, aes(x=pickup_longitude[1:100,1], y=pickup_latitude[1:100,2])) +
-  geom_point(size=1, color = "blue") +
-  scale_x_continuous(limits=c( -99.507504, -98.722426)) +
-  scale_y_continuous(limits=c(18.980224, 19.60)) +
-  theme_map()
+# Guardamos los resultados 
+write.csv(cdmx.rutas,"cdmx_rutas_municipios_save.csv")
