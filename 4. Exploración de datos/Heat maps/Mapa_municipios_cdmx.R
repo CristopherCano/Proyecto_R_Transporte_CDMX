7824164f0f3551a85038f835cb1c78e5dda8007e
Cristopher Cano <crisarvizu16@gmail.com>
2021-02-08 15:39:52 -0600

    update

--- 4. Exploración de datos/Heat maps/Mapa_municipios_cdmx.R
+++ 4. Exploración de datos/Heat maps/Mapa_municipios_cdmx.R
@@ -1,4 +1,4 @@
-### Origen
+(### Origen
 origen <- cdmx.rutas[,c("pickup_longitude","pickup_latitude")]
 head(origen)# Librerias utilizadas
 library(sf)                                 # Leer información geográfica
@@ -10,12 +10,30 @@ library(knitr)
 
 ## Leemos informacion 
 
+# Mapa de indicadores del Atlas de Riesgo de la CDMX
+mapa <- st_read("http://atlas.cdmx.gob.mx/datosAbiertos/INDICADORES_AGEB.geojson", quiet = T) %>% 
+  st_transform(crs = 4326)
+
+
 # Mapa de municipios de la CDMX
 mapa_municipios <- st_read("https://github.com/JuveCampos/Shapes_Resiliencia_CDMX_CIDE/raw/master/Zona%20Metropolitana/EdosZM.geojson", quiet = T) %>% 
   filter(CVE_ENT == "09")
 
 # Mapa de la entidad de la Ciudad de México
 mapa_cdmx <- st_read("https://github.com/JuveCampos/Shapes_Resiliencia_CDMX_CIDE/raw/master/Zona%20Metropolitana/EstadosZMVM.geojson", quiet = T)[3,]
+
+
+
+
+# Modificamos Base
+mapa$ENTIDAD <- "Ciudad de México"
+mapa$ALCALDIA <- as.factor(mapa$ALCALDIA)
+levels(mapa$ALCALDIA)[c(2, 3, 5, 13, 16)] <- c("Benito Juárez", "Coyoacaán", "Cuauhtémoc", "Tláhuac", "Álvaro Obregón")
+
+
+# Modificamos base, para quedarnos con factores
+a <- lapply(mapa, class) %>% unlist()
+mapaFactor <- mapa[,a == "factor"]
 
 
 # Cargamos la base de datos
@@ -40,6 +58,52 @@ municipios$lati <- lati
 
 library(leaflet.extras)
 
+### MAPA ORIGENES
+
+(map <- leaflet(mapaFactor, options = leafletOptions(zoomControl = FALSE, minZoom = 10)) %>%
+    addProviderTiles("CartoDB.Positron") %>% 
+    setMaxBounds(lng1 = -99.607504, lat1 = 18.780224, lng2 = -98.722426, lat2 = 19.63) %>%
+    addPolygons(data = mapa_municipios, 
+                color = "#444444",
+                weight = 2, 
+                opacity = 0.4,
+                fill = F,
+                label = ~as.character(NOM_MUN)) %>%
+    addPolygons(data = mapa_cdmx, 
+                color = "#444444",
+                weight = 4, 
+                opacity = 1,
+                fill = F
+    )) %>% addMarkers(lng=origen[1:5500,1], lat=origen[1:5500,2],  clusterOptions = markerClusterOptions()) %>%
+  addPopups(lng=municipios$lngt, lat=municipios$lati, municipios$mapa_municipios.NOM_MUN,options = popupOptions(closeButton = FALSE), options(popupOptions(
+    maxHeight = 0.2)))
+  
+
+### MAPA DESTINOS
+
+(map <- leaflet(mapaFactor, options = leafletOptions(zoomControl = FALSE, minZoom = 10)) %>%
+    addProviderTiles("CartoDB.Positron") %>% 
+    setMaxBounds(lng1 = -99.607504, lat1 = 18.780224, lng2 = -98.722426, lat2 = 19.63) %>%
+    addPolygons(data = mapa_municipios, 
+                color = "#444444",
+                weight = 2, 
+                opacity = 0.4,
+                fill = F,
+                label = ~as.character(NOM_MUN)) %>%
+    addPolygons(data = mapa_cdmx, 
+                color = "#444444",
+                weight = 4, 
+                opacity = 1,
+                fill = F
+    )) %>% addMarkers(lng=destino[1:5500,1], lat=destino[1:5500,2],  clusterOptions = markerClusterOptions()) %>%
+  addPopups(lng=municipios$lngt, lat=municipios$lati, municipios$mapa_municipios.NOM_MUN,options = popupOptions(closeButton = FALSE), options(popupOptions(
+    maxHeight = 0.2)))
+
+
+
+### ORIGEN Y DESTINOS
+
+
 (map <- leaflet(mapaFactor, options = leafletOptions(zoomControl = FALSE, minZoom = 10)) %>%
     addProviderTiles("CartoDB.Positron") %>% 
     setMaxBounds(lng1 = -99.507504, lat1 = 18.980224, lng2 = -98.722426, lat2 = 19.60) %>%
@@ -58,10 +122,8 @@ library(leaflet.extras)
   addMarkers(lng=destino[1:5500,1], lat=destino[1:5500,2],  clusterOptions = markerClusterOptions()) %>%
   addPopups(lng=municipios$lngt, lat=municipios$lati, municipios$mapa_municipios.NOM_MUN,options = popupOptions(closeButton = FALSE), options(popupOptions(
     maxHeight = 0.2)))
-  
 
-
-
+### MAPAS EJEMPLO
 
 (map <- leaflet(mapaFactor, options = leafletOptions(zoomControl = FALSE, minZoom = 10)) %>%
     addProviderTiles("CartoDB.Positron") %>% 
@@ -81,3 +143,4 @@ library(leaflet.extras)
   addCircleMarkers(lng=destino[1:5500,1], lat=destino[1:5500,2], radius = .2, color = "#ff325b", opacity = 0.02) %>%
   addPopups(lng=municipios$lngt, lat=municipios$lati, municipios$mapa_municipios.NOM_MUN,options = popupOptions(closeButton = FALSE), options(popupOptions(
     maxHeight = 3)))
+
